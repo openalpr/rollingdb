5aee1fabe117bf37a0e26242423846d1b4e42159
Matt Hill <matthill@openalpr.com>
2017-08-18 12:04:29 -0400

    Added test utils

---
+++ rdb_read.cpp
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: mhill
+ *
+ * Created on August 18, 2017, 10:39 AM
+ */
+
+#include <cstdlib>
+#include <tclap/CmdLine.h>
+#include "src/system_utils.h"
+#include "src/rollingdb.h"
+
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/consoleappender.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+static log4cplus::Logger logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("rdb_read"));
+
+int main( int argc, const char** argv )
+{
+  std::string out_file;
+  std::string rollingdb_dir;
+  std::string key;
+
+
+  TCLAP::CmdLine cmd("RollingDB Read Data", ' ', "1.0");
+
+  TCLAP::ValueArg<std::string> rollingDbDirArg("r","rollingdb_dir","Path of the rollingdb (default ./)",false, "./" ,"rollingdb_dir");
+  TCLAP::ValueArg<std::string> outputFileArg("o","output_file","output location to write the file",true, "" ,"output_file");
+  
+  TCLAP::UnlabeledValueArg<std::string>  keyArg( "key", "Key for object in database", true, "", "key"  );
+
+
+  try
+  {
+    cmd.add( outputFileArg );
+    cmd.add( rollingDbDirArg );
+    cmd.add( keyArg );
+    
+
+    cmd.parse( argc, argv );
+
+
+    rollingdb_dir = rollingDbDirArg.getValue();
+    out_file = outputFileArg.getValue();
+    key = keyArg.getValue();
+  }
+  catch (TCLAP::ArgException &e)    // catch any exceptions
+  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    return 1;
+  }
+  
+  log4cplus::SharedAppenderPtr myAppender(new log4cplus::ConsoleAppender());
+  myAppender->setName("rdb_read");
+  logger.addAppender(myAppender);
+  
+  cout << "Loading RollingDB at location: " << rollingdb_dir << endl;
+  
+  const int MAX_DATABASE_SIZE_GB = 5;
+  RollingDB rollingdb(rollingdb_dir, MAX_DATABASE_SIZE_GB, logger, true);
+  
+  std::vector<unsigned char> file_bytes;
+  bool found_file = rollingdb.read_blob(key, file_bytes);
+  
+  if (!found_file)
+  {
+    cout << "Could not find key in database" << endl;
+    return 1;
+  }
+  
+  cout << "Writing data bytes to " << out_file << endl;
+  ofstream fout;
+  fout.open(out_file, ios::binary | ios::out);
+  fout.write((char*) file_bytes.data(), file_bytes.size());
+  fout.close();
+  
+  
+  return 0;
+}
+
---
+++ rdb_write.cpp
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: mhill
+ *
+ * Created on August 18, 2017, 10:39 AM
+ */
+
+#include <cstdlib>
+#include <tclap/CmdLine.h>
+#include "src/system_utils.h"
+#include "src/rollingdb.h"
+
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+#include <log4cplus/consoleappender.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+static log4cplus::Logger logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("rdb_write"));
+
+int main( int argc, const char** argv )
+{
+  std::vector<std::string> filenames;
+  std::string rollingdb_dir;
+
+
+  TCLAP::CmdLine cmd("RollingDB Write Data", ' ', "1.0");
+
+  TCLAP::UnlabeledMultiArg<std::string>  fileArg( "files", "Binary files to add to the database", true, "", "files"  );
+
+  TCLAP::ValueArg<std::string> rollingDbDirArg("r","rollingdb_dir","Path of the rollingdb (default ./)",false, "./" ,"rollingdb_dir");
+
+  try
+  {
+    cmd.add( fileArg );
+    cmd.add( rollingDbDirArg );
+    
+
+    cmd.parse( argc, argv );
+
+
+    filenames = fileArg.getValue();
+    rollingdb_dir = rollingDbDirArg.getValue();
+
+  }
+  catch (TCLAP::ArgException &e)    // catch any exceptions
+  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    return 1;
+  }
+  
+  log4cplus::SharedAppenderPtr myAppender(new log4cplus::ConsoleAppender());
+  myAppender->setName("rdb_write");
+  logger.addAppender(myAppender);
+  
+  cout << "Loading/Initializing RollingDB at location: " << rollingdb_dir << endl;
+  
+  const int MAX_DATABASE_SIZE_GB = 5;
+  RollingDB rollingdb(rollingdb_dir, MAX_DATABASE_SIZE_GB, logger, false);
+  
+  for (uint32_t i = 0; i < filenames.size(); i++)
+  {
+    if (rollingdbsupport::fileExists(filenames[i].c_str()) && !rollingdbsupport::DirectoryExists(filenames[i].c_str()))
+    {
+      uint64_t epoch_time = rollingdbsupport::getEpochTimeMs();
+      stringstream key_name;
+      key_name << filenames[i] << "-" << rollingdbsupport::getEpochTimeMs() ;
+      
+      cout << "Writing " << filenames[i] << " to RollingDB with key: " << key_name.str() << endl;
+      
+      std::vector<unsigned char> file_bytes = rollingdbsupport::ReadAllBytes(filenames[i].c_str());
+      rollingdb.write_blob(filenames[i], epoch_time, file_bytes);
+    }
+  }
+  
+  // Wait for asynchronous write thread to complete
+  while (rollingdb.get_write_buffer_size() > 0)
+    rollingdbsupport::sleep_ms(100);
+  
+  return 0;
+}
+
